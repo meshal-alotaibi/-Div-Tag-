b80314f3bcb6747a8766c4aa8a96f412575b6bb5
ghaidaakhalee <51974001+ghaidaakhalee@users.noreply.github.com>
2021-02-16 19:52:55 +0300

    starter code

---
+++ starter_code/main_app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+# import user model
+from django.contrib.auth.models import User
+class Board(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+    description = models.CharField(max_length=150)
+    # method convert object to string
+    def __str__(self) :
+        return self.name
+
+#  1=>m relation between Board and Topic
+#  one borde can have many topic 
+
+class Topic(models.Model):
+    #  1=>m relation between Board and Topic
+#  one borde can have many topic 
+    subject= models.CharField(max_length=150)
+    #  on_delete=> when user delet topic it will deleted from borde also
+    board = models.ForeignKey(Board,related_name='topics',on_delete=models.CASCADE)
+    created_by= models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
+    #  to add time auto use DateTimeField(auto_now_add=True)
+    created_dt =models.DateTimeField(auto_now_add=True)
+
+
+
+class Post(models.Model):
+    message= models.TextField(max_length=200)
+    topic= models.ForeignKey(Topic,related_name='posts',on_delete=models.CASCADE)   
+    created_by= models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE)
+    created_dt =models.DateTimeField(auto_now_add=True)
+
+class User(models.Model):
+    username= models.CharField(max_length=20)
+    password=models.CharField(max_length=20)
+    isAdmin : models.BooleanField
