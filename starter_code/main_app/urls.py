01fde6d87740e33d2b9fc0291e25a845dbf80794
12_Manar Mac_SEI-14 <mmanaralmutlaq@gmail.com>
2021-02-17 18:25:21 +0300

    post page , edit home , views , url, add css , js , bootStrap and img , add base page (#6)
    Co-authored-by: ManarAlmutlaq <>

--- starter_code/main_app/urls.py
+++ starter_code/main_app/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     # template_name to opent loin file that in template folder #name -->is the url for login page
-    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login')
-
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('category/<int:category_id>/',
+         views.category_topics, name='category_topics'),
+    path('category/<int:category_id>/topics/<int:topic_id>/',
+         views.topic, name='topic'),
 ]
--- starter_code/main_app/views.py
+++ starter_code/main_app/views.py
@@ -1,23 +1,28 @@
-
+from django.http.response import Http404
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
-from django.shortcuts import render
-from .models import Category
+from django.shortcuts import render , get_object_or_404
+from .models import Category , Topic
 
 # Create your views here.
 
 def index(request):
     category = Category.objects.all()
+    topic = Topic.objects.all()
     print(category)
-    return render(request, 'home.html', {'categories':  category})
+    return render(request, 'home.html', {'categories': category , 'topics':topic})
 
 
+def category_topics(request, category_id):
+    categories = Category.objects.all()
+    category = get_object_or_404(Category ,pk=category_id)
+    return render(request, 'topics.html', {'categories': categories, 'category': category})
 
 
-# Create your views here.
-
+def topic(request, category_id , topic_id):
+    topic = get_object_or_404(Topic, category__pk=category_id, pk=topic_id)
+    return render(request, 'topic.html', {'topic':topic})
 def signup(request):
     form=UserCreationForm()
     if request.method =='POST':
