85f633986939bc71d81d2c0449147dca010df009
saudsaadotb <saudsaadotb@gmail.com>
2021-02-17 21:06:30 +0300

    test

--- starter_code/main_app/urls.py
+++ starter_code/main_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.index, name='index'),
+    psth('logout/',auth_views.LogoutView.as_view().name='logout')
+    
 ]
--- starter_code/starter_code/settings.py
+++ starter_code/starter_code/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blogs',
-        'PASSWORD':'0564305522',
+        'PASSWORD':'1567',
         'USER': 'postgres',
     }
 }
