96af87fd99c0725fcc511cc5baace86864aec0f5
12_Manar Mac_SEI-14 <mmanaralmutlaq@gmail.com>
2021-02-17 22:50:49 +0300

    form , home , topic,edit model , edit views , edit setting (#8)
    Co-authored-by: ManarAlmutlaq <>
    Co-authored-by: ghaidaakhalil <ghaidaa_khalil@outlook.com>

--- starter_code/main_app/models.py
+++ starter_code/main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # import user model
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -20,17 +20,20 @@ class Topic(models.Model):
     #  1=>m relation between Board and Topic
     #  one borde can have many topic 
     subject= models.CharField(max_length=150)
+    content_text = models.TextField(max_length=3500)
     #  on_delete=> when user delet topic it will deleted from borde also
     category = models.ForeignKey(Category,related_name='topics',on_delete=models.CASCADE)
     created_by= models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
     #  to add time auto use DateTimeField(auto_now_add=True)
-    created_dt =models.DateTimeField(auto_now_add=True)
+    created_dt = models.DateTimeField(default=timezone.now)
+    
+
+
 
     # method convert object to string
 
     def __str__(self):
        return self.subject
-    
 
 
 
@@ -38,7 +41,7 @@ class Post(models.Model):
     message= models.TextField(max_length=200)
     topic= models.ForeignKey(Topic,related_name='posts',on_delete=models.CASCADE)   
     created_by= models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE)
-    created_dt =models.DateTimeField(auto_now_add=True)
+    created_dt = models.DateTimeField(default=timezone.now)
      
     # method convert object to string
 
