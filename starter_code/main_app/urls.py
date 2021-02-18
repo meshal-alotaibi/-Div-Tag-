ae0a42298887951607cdd40cdb356e85ebc40653
saudsaadotb <saudsaadotb@gmail.com>
2021-02-18 11:08:11 +0300

    logout and dropdown manu

--- starter_code/main_app/urls.py
+++ starter_code/main_app/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
-<<<<<<< HEAD
-=======
 from django.contrib.auth import views as auth_views
->>>>>>> 9f7a62ff02cb6321eae1e4eded43e5387cbf4b35
 from . import views
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-<<<<<<< HEAD
-    psth('logout/',auth_views.LogoutView.as_view().name='logout')
-    
-]
-=======
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     # template_name to opent loin file that in template folder #name -->is the url for login page
@@ -19,6 +12,7 @@ urlpatterns = [
          views.category_topics, name='category_topics'),
     path('category/<int:category_id>/topics/<int:topic_id>/',
          views.topic, name='topic'),
+    path('logout/', auth_views.LogoutView.as_view(),name='logout')
+
 
 ]
->>>>>>> 9f7a62ff02cb6321eae1e4eded43e5387cbf4b35
